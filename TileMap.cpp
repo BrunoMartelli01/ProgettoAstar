1a48c5aa90d44c97d8899f3285f51d4894d0e77f
bruno <bruno.martelli2001@gmail.com>
2022-07-28 20:10:41 +0200

    Aggiunta miglioramento della wordMap e fix dei size dei tile

--- TileMap.cpp
+++ TileMap.cpp
@@ -45,13 +45,13 @@ bool TileMap::load(const std::string &tileset, sf::Vector2u tileSize, const Worl
             sf::Vertex *quad = &m_vertices[(i + j * width) * 4];
 
             // define its 4 corners
-            quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
-            quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-            quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
-            quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+            quad[0].position = sf::Vector2f(i * tileSize.x/2 , j * tileSize.y/2);
+            quad[1].position = sf::Vector2f((i + 1) * tileSize.x/2, j * tileSize.y/2);
+            quad[2].position = sf::Vector2f((i + 1) * tileSize.x/2, (j + 1) * tileSize.y/2);
+            quad[3].position = sf::Vector2f(i * tileSize.x/2, (j + 1) * tileSize.y/2);
 
             // define its 4 texture coordinates
-            quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+            quad[0].texCoords = sf::Vector2f(tu * tileSize.x , tv * tileSize.y);
             quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
             quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
             quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
